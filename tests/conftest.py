4076e79923e2d7b1667cec1d4a1a03d76519ad65
Denzel <butmanovich@yandex.ru>
2025-10-19 12:58:29 +0300

    adding fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,8 @@ def in_browser():
     browser.config.driver_name = 'chrome'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com'
+
+    yield
+
+    browser.quit()
