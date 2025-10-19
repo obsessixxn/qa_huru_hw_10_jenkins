7d47851ac9799cb46e5a83675a9c3b70a8a34c9f
Denzel <butmanovich@yandex.ru>
2025-10-19 13:08:31 +0300

    adding fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ def in_browser():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 10
 
     yield browser
 
