373bb1cdba434633a00e4041c43bbf1624a91260
Denzel <butmanovich@yandex.ru>
2025-10-19 13:12:33 +0300

    adding fixture

--- tests/test/test_registration.py
+++ tests/test/test_registration.py
@@ -1,41 +1,58 @@
+import os
 import time
 
-from selene import browser
+from selene import browser, have
 from selene.support.shared import browser
-
-from registration_form import RegistrationFormPage
 
 
 def test_sending_form(in_browser):
+    browser.open('https://demoqa.com/automation-practice-form')
 
-    register_page = RegistrationFormPage()
-    register_page.open("https://demoqa.com/automation-practice-form")
-    register_page.fill_name("Daniil")
-    register_page.fill_surname("Zhuravlev")
-    register_page.fill_email("butmanovich@yandex.ru")
-    register_page.choose_gender(1)
-    register_page.fill_number("3123131232")
-    register_page.fill_date_of_birth('2002', 'May', '11')
-    register_page.choose_subject("Computer Science")
-    register_page.choose_hobby()
-    register_page.choose_photo('123123.png')
-    register_page.fill_address('Moskva, street 10')
-    register_page.choose_state('NCR')
-    register_page.choose_city('Delhi')
-    register_page.submit_form()
-    register_page.should_registered_student_with(
-        "Daniil Zhuravlev",
-        "butmanovich@yandex.ru",
-        "Male",
-        "3123131232",
-        "11 May,2002",
-        'Computer Science',
-        'Sports',
-        '123123.png',
-        'Moskva, street 10',
-        'NCR',
-        'Delhi'
-    )
+    browser.element("#firstName").type("Daniil")
+    browser.element("#lastName").type("Zhuravlev")
+    browser.element("#userEmail").type("butmanovich@yandex.ru")
 
+    browser.element('[for="gender-radio-1"]').click()
+
+    browser.element("#userNumber").type("7999512555")
+
+    browser.element("#dateOfBirthInput").click()
+    browser.element("option[value='2'").click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="2002"]').click()
+    browser.element('.react-datepicker__day--023').click()
+
+    browser.element("#subjectsInput").type("English").press_enter()
+    browser.element("#subjectsInput").type("Computer Science").press_enter()
+
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+
+    browser.element("#uploadPicture").set_value(os.path.abspath('resources/123123.png'))
+
+    browser.element('#currentAddress').type("г.Москва, Покровка, 5")
+
+    browser.element('#state').click()
+    browser.all('div[class*="option"]').element_by(have.text('NCR')).click()
+
+    browser.element('#city').click()
+    browser.all('div[class*="option"]').element_by(have.text('Delhi')).click()
+
+    browser.element('#submit').click()
+
+    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
+
+    browser.all('td').should(have.texts([
+        'Student Name', 'Daniil Zhuravlev',
+        'Student Email', 'butmanovich@yandex.ru',
+        'Gender', 'Male',
+        'Mobile', '7999512555',
+        'Date of Birth', '23 March,2002',
+        'Subjects', 'English, Computer Science',
+        'Hobbies', 'Sports',
+        'Picture', '123123.png',
+        'Address', 'г.Москва, Покровка, 5',
+        'State and City', 'NCR Delhi'
+    ]
+    ))
     browser.element('#closeLargeModal').click()
     time.sleep(2)
