70cee2281795eb2dd0e85360fbc271b9379a6907
Denzel <butmanovich@yandex.ru>
2025-10-19 21:40:10 +0300

    adding selenoud ui for jenkins

--- tests/test/conftest.py
+++ tests/test/conftest.py
@@ -8,15 +8,27 @@ from selenium import webdriver
 @pytest.fixture(scope='session')
 def in_browser():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--window-size=1920,1080")
 
     options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
-    driver = webdriver.Chrome(options=options)
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVideo": True,
+            "enableVNS": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
     browser = Browser(
         Config(
