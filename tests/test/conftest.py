29f8e2f5481a7dd6ce4d5ea864df93a0af402a91
Denzel <butmanovich@yandex.ru>
2025-10-19 13:51:34 +0300

    adding fixture

--- tests/test/conftest.py
+++ tests/test/conftest.py
@@ -1,23 +1,31 @@
 import tempfile
 
 import pytest
-from selene import browser
+from selene import browser, Config
 from selenium import webdriver
 
 
 @pytest.fixture(scope='session')
 def in_browser():
     options = webdriver.ChromeOptions()
-    browser.config.driver_name = 'chrome'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 10
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
     options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
-    # browser.config.driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
+    browser.config = Config(
+        driver=driver,
+        base_url="https://demoqa.com",
+        timeout=6,
+        window_width=1920,
+        window_height=1080,
+    )
 
+    yield browser
+
+    browser.quit()
--- tests/test/test_registration.py
+++ tests/test/test_registration.py
@@ -6,6 +6,7 @@ from selene.support.shared import browser
 
 
 def test_sending_form(in_browser):
+    browser = in_browser
     browser.open("https://demoqa.com/automation-practice-form")
 
     browser.element("#firstName").type("Daniil")
